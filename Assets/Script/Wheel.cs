5c453f6818b3a5653882feddbad7d1f2861f2869
yevhenkr <vasilenkoevgen88@gmail.com>
2021-03-05 14:00:51 +0200

    add

---
+++ Assets/Script/Wheel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Wheel : MonoBehaviour
+{
+    private float _anglePart ;
+
+    public byte CountParts;
+
+    private void Awake()
+    {
+        _anglePart = 360 / CountParts;
+    }
+
+//    public StateWheel = StateWheel.I//todo
+    void Update()
+    {
+//        Debug.Log("transform.eulerAngles.z = " +transform.eulerAngles.z);
+        Debug.Log("Math = " + Math.Truncate(transform.eulerAngles.z / _anglePart));
+    }
+    
+    //todo крутится быстро медленно всегда рандом отедльный клас крутящий момент 
+    //todo manager связное звено между колесом и выводом на экран значения
+    //todo где мы в этом классе getSelectPart
+    
+}
+
+public enum StateWheel
+{
+    Idle,
+    Run
+}
---
+++ Assets/Script/WheelRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotation : MonoBehaviour
+{
+    public Wheel While;
+    private byte _timeRotaition;
+    
+    
+    
+}
